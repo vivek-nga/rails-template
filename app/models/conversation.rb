b4d3ef2c5de3b9a47e5d0d0b93989d9424b0c051
Vivek Singh <vivek.singh@cardinaltsinc.net>
2018-05-09 19:25:27 +0530

    Added actioncable

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -1,7 +1,17 @@
 class Conversation < ApplicationRecord
-  has_secure_token :key
+  after_initialize :generate_key
   belongs_to :to_user, class_name: 'User', foreign_key: 'chat_to', inverse_of: :conversations_from, optional: true
   belongs_to :from_user, class_name: 'User', foreign_key: 'chat_from', inverse_of: :conversations_to, optional: true 
   has_many :chats
 
+  def generate_key
+    return if self.key.present?
+    key = nil
+    loop do
+      key = SecureRandom.hex(10)
+      break key unless self.class.where(key: key).exists?
+    end
+    self.key = key
+  end
+  
 end
